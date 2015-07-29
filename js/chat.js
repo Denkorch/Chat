58c8fcf5a915a5b2e154bd8ffa8ce5e9992cf03c
Denys Korcheniuk <denniskorch@gmail.com>
2015-07-29 20:13:08 +0300

    else

--- js/chat.js
+++ js/chat.js
@@ -55,6 +55,9 @@ $(document).ready(function() {
             		document.cookie = "userName = " + login;
             		document.cookie = "userId = " + data[0].id;
             		window.location.href = "http://denkorch.github.io";
+            		//додати ім`я юзера
+            		$('.header').append('<span class="" style:"display:inline-block;position:absolute;right:0;top:0;height:50px;color:#fff;">' +
+            		+ getCookie("userName") + '</span>')
             	} else {
             		window.location.reload();
             	};
