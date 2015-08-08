2f121fbba8ef81ed73e20c36e0e53e1a851473ca
Denys Korcheniuk <denniskorch@gmail.com>
2015-08-08 15:48:05 +0300

    test

--- js/chat.js
+++ js/chat.js
@@ -192,7 +192,7 @@ $(document).ready(function() {
             		if (!(name in myConversations)) {
             				// myConversations[name] = name;
             				myConversations[name] = messages(name);
-            				for (var i = 0; i < myConversations.eq(); i++) {
+            				for (var i = 0; i < myConversations.keys().length; i++) {
             					$('.message-list').append('<li class="list-group-item li-message-list">' + myConversations.eq(i) + '</li>');
             				};
             			};
