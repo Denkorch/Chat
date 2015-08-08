6dc69e3f14f75dc0a601e2db9db5bfaedf6fe099
Denys Korcheniuk <denniskorch@gmail.com>
2015-08-08 15:24:34 +0300

    test

--- js/chat.js
+++ js/chat.js
@@ -195,7 +195,7 @@ $(document).ready(function() {
             			};
             			for (var i = 0; i < myConversations.length; i++) {
             				myConversations[i]
-            				$('.message-list').append('<li class="list-group-item li-message-list">' + myConversations[i] + '</li>');
+            				$('.message-list').append('<li class="list-group-item li-message-list">' + myConversations[name] + '</li>');
             			};
 					// $('.message-list').append('<li class="list-group-item li-message-list">' + data[i].name_sender + '<span class="msg-time">' + data[i].create_date + '</span>'  + '</li>');
 				};
