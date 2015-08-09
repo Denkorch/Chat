a79de6408451f54cd6b701ef7e044397f0654ff9
Denys Korcheniuk <denniskorch@gmail.com>
2015-08-09 18:41:36 +0300

    test

--- js/chat.js
+++ js/chat.js
@@ -210,14 +210,14 @@ $(document).ready(function() {
 				$( ".li-message-list" ).on( "click", function() {
 					var item = $(this).index()-1;
 					var nick = Object.getOwnPropertyNames(myConversations)[item];
-					function ulItems(arg) {
-					 	for (var i = 0; i < Object.keys(myConversations[arg]).length; i++) {
-            				$(".msg-ul").append('<li class="list-group">' + myConversations[arg][i]["message"] + '</li>');
+					var ulItems = function (nick) {
+					 	for (var i = 0; i < Object.keys(myConversations[nick]).length; i++) {
+            				$(".msg-ul").append('<li class="list-group">' + myConversations[nick][i]["message"] + '</li>');
             				// console.log(myConversations[arg][i]["message"]);
             			};
 					};
 					$(".msg_form").css("display", "none");
-					$(".msg-area").append('<div class="well well-lg">' + '<ul class="list-group msg-ul">' + ulItems(nick) + '</ul>' + '</div>');
+					$(".msg-area").append('<div class="well well-lg">' + '<ul class="list-group msg-ul">' + ulItems() + '</ul>' + '</div>');
 				});
 			},
 			error: function() {
